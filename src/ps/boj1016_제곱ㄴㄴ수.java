f4e257bf7718ae31674858bfcf76f16db8b8478d
jig7357 <jig7357@naver.com>
2020-09-06 23:47:55 +0900

    200906

---
+++ src/ps/boj1016_제곱ㄴㄴ수.java
@@ -0,0 +1,14 @@
+package ps;
+
+import java.util.Scanner;
+
+public class boj1016_제곱ㄴㄴ수 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int min = sc.nextInt();
+		int max = sc.nextInt();
+		
+		
+	}
+}
