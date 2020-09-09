937c3d037e0cc9196b3d9e8ad73e31df81230274
jig7357 <jig7357@naver.com>
2020-09-10 00:55:27 +0900

    200909

--- src/ps/programmers_비밀지도.java
+++ src/ps/programmers_비밀지도.java
@@ -1,25 +1,16 @@
-import java.util.*;
+package ps;
 
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
-        int[] res = new int[n];
-        
         for(int i=0;i<n;i++){
-            res[i] = (arr1[i] | arr2[i] | (1<<n));
+            arr1[i] = (arr1[i] | arr2[i] | (1<<n));
         }
-        
         String[] ans = new String[n];
         for(int i=0;i<n;i++){
-            char[] str = Integer.toBinaryString(res[i]).toCharArray();
-            
-            String temp = "";
-            for(int j=1;j<=n;j++){
-                if(str[j] == '1') temp += '#';
-                else if(str[j] == '0') temp += ' ';
-            }
-            ans[i] = temp;
+            String str = Integer.toBinaryString(arr1[i]).substring(1);
+            str = str.replaceAll("1", "#");
+            ans[i] = str.replaceAll("0", " "); 
         }
-        
         return ans;
     }
 }
