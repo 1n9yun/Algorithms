87bd1a930700ea52d2341e071c9aa0d0073ad95d
1n9yun <jig7357@gmail.com>
2020-10-19 10:28:18 +0900

    update

--- src/ps/CodingTest/Coupang20201009/_01.java
+++ src/ps/CodingTest/Coupang20201009/_01.java
@@ -1,15 +1,32 @@
 package ps.CodingTest.Coupang20201009;
+import java.util.Arrays;
 
 public class _01 {
 	public static void main(String[] args) {
-		solution(3);
+		solution(1000000);
 	}
 	
 	static public int[] solution(int N) {
-		int[] answer = {};
 
-		System.out.println(getTransNum(5, 2));
+		int max = -1;
+		int maxK = 0;
 		
+		for(int i=10;i>1;i--) {
+			String transformedNum = getTransNum(N, i);
+			
+			int multi = 1;
+			for(int j=0;j<transformedNum.length();j++) {
+				multi *= (transformedNum.charAt(j) - '0');
+			}
+			
+			if(max < multi) {
+				max = multi;
+				maxK = i;
+			}
+		}
+		
+		int[] answer = {maxK, max};
+		System.out.println(Arrays.toString(answer));
 		return answer;
 	}
 	
@@ -21,6 +38,7 @@ public class _01 {
 			sb.append(n % k);
 			n /= k;
 		}
+		sb.append(n);
 		
 		sb.reverse();
 		return sb.toString();
--- src/ps/CodingTest/Coupang20201009/_04.java
+++ src/ps/CodingTest/Coupang20201009/_04.java
@@ -1,5 +1,93 @@
 package ps.CodingTest.Coupang20201009;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Queue;
 
 public class _04 {
-
+	
+	public static void main(String[] args) {
+		
+		String[][] roads = {{"ULSAN","BUSAN"},{"DAEJEON","ULSAN"},{"DAEJEON","GWANGJU"},{"SEOUL","DAEJEON"},{"SEOUL","ULSAN"},{"DAEJEON","DAEGU"},{"GWANGJU","BUSAN"},{"DAEGU","GWANGJU"},{"DAEGU","BUSAN"},{"ULSAN","DAEGU"},{"GWANGJU","YEOSU"},{"BUSAN","YEOSU"}};
+		
+		System.out.println(solution("SEOUL", "DAEGU", "YEOSU", roads));
+	}
+	
+	static final int DIV = 10007;
+	static int solution(String depar, String hub, String dest, String[][] roads) {
+		Map<String, Integer> indexing = new HashMap<>();
+		
+		int index = 0;
+		for(int i=0;i<roads.length;i++) {
+			if(!indexing.containsKey(roads[i][0])) {
+				indexing.put(roads[i][0], index++);
+			}
+			if(!indexing.containsKey(roads[i][1])) {
+				indexing.put(roads[i][1], index++);
+			}
+			
+			System.out.println(roads[i][0] + ": " + indexing.get(roads[i][0]));
+			System.out.println(roads[i][1] + ": " + indexing.get(roads[i][1]));
+		}
+		
+		ArrayList<Integer>[] adjList = new ArrayList[index];
+		for(int i=0;i<index;i++) adjList[i] = new ArrayList<>();
+		
+		int[] come = new int[index];
+		for(int i=0;i<roads.length;i++) {
+			int from = indexing.get(roads[i][0]);
+			int to = indexing.get(roads[i][1]);
+			
+			adjList[from].add(to);
+			come[to]++;
+		}
+		
+		int[][] dp = new int[index][2];
+		int start = indexing.get(depar);
+		int mid = indexing.get(hub);
+		int end = indexing.get(dest);
+		
+		int[] count = new int[index];
+		boolean[] check = new boolean[index];
+		
+		Queue<Integer> q = new ArrayDeque<>();
+		for(int i=0;i<adjList[start].size();i++) {
+			int to = adjList[start].get(i);
+			dp[to][0] = 1;
+			q.add(to);
+			count[to]++;
+		}
+		System.out.println(Arrays.toString(count));
+		while(!q.isEmpty()) {
+			int front = q.poll();
+			if(come[front] != count[front] || check[front]) {
+				System.out.println(front + "-> " + come[front] + "!=" + count[front]);
+				continue;
+			}else {
+				System.out.println(front + "통과-> " + come[front] + "!=" + count[front]);
+				check[front] = true;
+			}
+			System.out.println("from : " + front);
+			
+			for(int i=0;i<adjList[front].size();i++) {
+				int to = adjList[front].get(i);
+				count[to]++;
+				System.out.print("to (" + to + ") ");
+				
+				dp[to][0] = (dp[to][0] + dp[front][0]) % DIV;
+				if(front == mid) dp[to][1] = (dp[to][1] + dp[front][0]) % DIV;
+				else dp[to][1] = (dp[to][1] + dp[front][1]) % DIV;
+				
+				System.out.println(dp[to][0] + " " + dp[to][1]);
+				
+				q.add(to);
+			}
+		}
+		
+		
+		
+		return dp[end][1];
+	}
 }
