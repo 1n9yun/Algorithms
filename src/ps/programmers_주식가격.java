7b5a12b84c42a33309cf6d9a8e54a4aacead7d29
1n9yun <jig7357@gmail.com>
2020-11-30 23:24:51 +0900

    201130

---
+++ src/ps/programmers_주식가격.java
@@ -0,0 +1,16 @@
+package ps;
+
+import java.util.Arrays;
+
+public class programmers_주식가격 {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(solution(new int[]{1, 2, 3, 2, 3})));
+    }
+    public static int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+
+        
+
+        return answer;
+    }
+}
