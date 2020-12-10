006400981f78865c56bc5d0453f991779436b908
1n9yun <jig7357@gmail.com>
2020-12-11 02:43:10 +0900

    201211

--- src/ps/boj13306_트리.java
+++ src/ps/boj13306_트리.java
@@ -7,6 +7,15 @@ import java.util.List;
 import java.util.StringTokenizer;
 
 public class boj13306_트리 {
+    static class Item{
+        int type, n1, n2;
+
+        public Item(int type, int n1, int n2) {
+            this.type = type;
+            this.n1 = n1;
+            this.n2 = n2;
+        }
+    }
     static int[] vertexSet;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -23,26 +32,32 @@ public class boj13306_트리 {
         for(int i=1;i<n;i++)
             link[i+1] = Integer.parseInt(br.readLine());
 
-        List<Integer> queries = new ArrayList<>();
+        List<Item> inputs = new ArrayList<>();
         for(int i=0;i<n-1+q;i++){
             st = new StringTokenizer(br.readLine());
 
             int type = Integer.parseInt(st.nextToken());
-            if(type == 0) link[Integer.parseInt(st.nextToken())] = -1;
-            else {
-                queries.add(Integer.parseInt(st.nextToken()));
-                queries.add(Integer.parseInt(st.nextToken()));
-            }
+            int n1 = -1, n2 = -1;
+            n1 = Integer.parseInt(st.nextToken());
+            if(type == 0) link[n1] *= -1;
+            else n2 = Integer.parseInt(st.nextToken());
+
+            inputs.add(new Item(type, n1, n2));
         }
 
         for(int i=2;i<link.length;i++){
-            if(link[i] == -1) continue;
+            if(link[i] < 0) continue;
             union(i, link[i]);
         }
-        System.out.println(Arrays.toString(vertexSet));
-        for(int i=0;i<queries.size();i+=2){
-            System.out.println(find(queries.get(i)) == find(queries.get(i+1)) ? "YES" : "NO");
+
+        List<String> answerList = new ArrayList<>();
+        for(int i=inputs.size() - 1;i>=0;i--){
+            Item item = inputs.get(i);
+            if(item.type == 0) union(item.n1, link[item.n1] * -1);
+            else answerList.add(find(item.n1) == find(item.n2) ? "YES" : "NO");
         }
+        for(int i=answerList.size()-1;i>=0;i--) bw.write(answerList.get(i) + "\n");
+        bw.flush();
     }
 
     static int find(int idx){
