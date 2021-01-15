fed80e8f6f647e7daee8873b45b4642ed273e16b
1n9yun <jig7357@gmail.com>
2021-01-15 23:41:05 +0900

    210115

---
+++ src/ps/boj1800_인터넷설치.java
@@ -0,0 +1,7 @@
+package ps;
+
+public class boj1800_인터넷설치 {
+    public static void main(String[] args) {
+
+    }
+}
--- src/ps/test.java
+++ src/ps/test.java
@@ -5,8 +5,19 @@ import java.util.*;
 
 public class test {
 	public static void main(String[] args) throws UnsupportedEncodingException {
-		System.out.println(Long.MAX_VALUE);
-		System.out.println(Double.MAX_VALUE);
+		List<Integer> list = new ArrayList<>();
 
+		list.add(1);
+		list.add(4);
+		list.add(7);
+		list.add(10);
+
+		for(Integer n : list){
+			n = n + 1;
+		}
+
+		for(Integer n : list){
+			System.out.print(n + " ");
+		}
 	}
 }
